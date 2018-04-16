b720bc2b00ff086e5ad5df751f4c51996f2400ae
noth1ng1strue <jamesdjackson@gmail.com>
2018-04-16 18:38:24 -0400

    SecondTest

---
+++ app/src/main/java/com/nothingistrue/Minted/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nothingistrue.Minted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
